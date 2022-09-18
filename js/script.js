84dd509c62bd82782a68c98139e6df90758b1bf7
Sergey Yakimovich <yakimovichsergei@gmail.com>
2022-09-18 19:23:07 +0300

    added files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function() {
+        const calc = require('./modules/calc'),
+              cards = require('./modules/cards'),
+              form = require('./modules/form'),
+              modal = require('./modules/modal'),
+              slider = require('./modules/slider'),
+              tabs = require('./modules/tabs'),
+              timer = require('./modules/timer');   
+        
+
+    
+        calc();
+        cards();
+        form();
+        modal();
+        slider();
+        tabs();
+        timer();   
+
+});
